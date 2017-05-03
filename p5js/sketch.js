17edabc77c93483e2af3fd3d3be90e17ac5f766d
Amelia <ariely@etsy.com>
2017-05-03 17:51:41 -0400

    An explanation for the baby animation

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -9,24 +9,8 @@ function draw() {
   //runs 60 times a second
 
   fill(random()*255, random()*255, random()*255, 100);
-  // red, green, blue, alpha (opacity)
   ellipse(random()*600, random()*600, 25, 25);
 }
-
-// function draw() {
-//   theta += 1;
-//   if (theta % 5 == 0) {
-//     fill(random()*255, random()*255, random()*255, random()*100);
-//     ellipse(mouseX, mouseY, 25, 25);
-//   }
-// }
-
-// function mouseMoved() {
-//   var norm_size = norm(mouseX, 0, width);
-//   // fill(mouseX*255, random()*255, random()*255, random()*100);
-//   stroke(255);
-//   ellipse(mouseX, mouseY, norm_size*100, norm_size*100);
-// }
 
 function keyPressed() {
     clear();
