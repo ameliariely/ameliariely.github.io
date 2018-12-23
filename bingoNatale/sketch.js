00adaedcfd4c781c23b496a3419d516022da0819
Amelia Riely <ariely@lyft.com>
2018-12-23 17:42:34 +0100

    Creando il bingo di natale con Leo

---
+++ bingoNatale/sketch.js
@@ -0,0 +1,60 @@
+var x;
+var i = -1;
+var estratti = [];
+var numeri = [];
+
+function setup() {
+  createCanvas(400, 400);
+	createNumeri();
+}
+
+function draw() {
+	if (i >= 0) {
+		fill(i, 255-i, 0);
+		ellipse(random()*300+30, random()*300+30, 25, 25);
+		i++;
+		if (i == 300) {
+			clear();
+			fill(0);
+			textSize(100);
+			text(estrai(), 200, 200);
+			textSize(15);
+			stampaEstratti();
+			i = -1;
+		}
+	}
+}
+
+function keyPressed() {
+	i = 0;
+	clear();
+}
+
+function estrai() {
+	var index = Math.floor(random() * numeri.length);
+	console.log(numeri.length);
+	var estratto = numeri[index];
+	numeri.splice(index, 1);
+	estratti.push(estratto);
+	estratti.sort(function(a, b){return a-b});
+	return estratto;
+}
+
+function createNumeri() {
+	var j;
+	for (j = 0; j < 90; j++) {
+		numeri.push(j+1);
+	}
+}
+
+function stampaEstratti() {
+	var string = "";
+	var indexEstratti;
+	for (indexEstratti = 0; indexEstratti < estratti.length; indexEstratti++) {
+		string += estratti[indexEstratti] + " ";
+		if ((indexEstratti + 1) % 20 == 0) {
+			string += '\n';
+		}
+	}
+	text(string, 30, 330);
+}
