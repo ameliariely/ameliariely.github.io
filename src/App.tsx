ca0b1530fdfb57129aa806addc5512d2312f5da7
ameliariely <riely@pushd.com>
2025-04-01 14:32:14 -0400

    switch to react app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import video from './videos/gorge.mp4';
+
+function App() {
+  return (
+    <div className="App">
+      <video autoPlay loop muted className="background-video">
+        <source src={video} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <h1>Amelia Riely</h1>
+    </div>
+  );
+}
+
+export default App;
