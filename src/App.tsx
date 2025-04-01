d1724fa16158d8cd9053353e322baf7c2a76ac12
ameliariely <riely@pushd.com>
2025-04-01 18:30:17 -0400

    fix video on phone

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import './App.css';
 import video from './assets/videos/gorge.mp4';
+import React, { useRef, useEffect } from 'react';
+
+function AutoPlaySilentVideo(props: { video: string; }) {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  useEffect(() => {
+      if (videoRef.current) {
+          videoRef.current.defaultMuted = true;
+      }
+  }, []);
+  return (
+      <video
+          ref={videoRef}
+          loop
+          autoPlay
+          muted
+          playsInline>
+          <source src={props.video} type="video/mp4"/>
+      </video>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <video autoPlay loop muted className="background-video">
-        <source src={video} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <AutoPlaySilentVideo video={video} />
       <h1>Amelia Riely</h1>
     </div>
   );
